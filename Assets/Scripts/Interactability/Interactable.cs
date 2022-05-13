d8c48aa65039e3ca3fbe9d018100a905a23f1b36
CypherKn1ght <37490057+CypherKn1ght@users.noreply.github.com>
2022-05-13 14:26:56 -0700

    Timer

---
+++ Assets/Scripts/Interactability/Interactable.cs
@@ -0,0 +1,17 @@
+/* using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    public abstract void Interact();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            
+        }
+    }
+}
+*/
