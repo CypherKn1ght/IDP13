5de3b883cd028d8d40c84297169a026cff729857
CasualCasuals <laureanoleandro247@gmail.com>
2022-05-20 13:49:54 -0700

    Entry House Portal completed

--- Assets/Scripts/Phase_2.cs
+++ Assets/Scripts/Phase_2.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Phase_2 : MonoBehaviour
 {
     public int sceneBuildIndex;
-    public bool tapedFingerprint = True;
+    public bool tapedFingerprint = true;
     // Level moved zone enter, if collider is a player
     //Move game to another scene
 
     private void OnTriggerEnter2D(Collider2D other) {
         print("Trigger Entered");
-        if(other.tag == "Player")&(tapedFingerprint == True){
-            print("Swithcing Scene to " + sceneBuildIndex);
-            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        if(other.tag == "Player"){
+            if(tapedFingerprint == true){
+                print("Swithcing Scene to " + sceneBuildIndex);
+                SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+            }
         }
     }
 }
