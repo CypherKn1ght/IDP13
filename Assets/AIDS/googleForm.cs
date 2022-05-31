823a3d1b7daa2a480f644f23ddb5c95afcf33273
CypherKn1ght <37490057+CypherKn1ght@users.noreply.github.com>
2022-05-31 14:38:47 -0700

    aids

---
+++ Assets/AIDS/googleForm.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class urlbutton : MonoBehaviour
+{
+  public void OpenURL()
+  {
+    Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSfI4FOWvBid9jHN7ZOC2RHZPosnU8zGodhcfcvhr48AEkywZw/viewform");
+  }
+}
+
